f3946ff85f49519176ee4353c44ec7de61502f5c
leon trestman <tleonleont@gmail.com>
2021-06-26 23:13:17 +0300

    last updare

--- Shifts/urls.py
+++ Shifts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
 
 app_name="Shifts"
 
@@ -8,3 +9,5 @@ urlpatterns =[
     path("", views.index, name="index"),
     path("addshift", views.addshift, name="addshift")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- ShiftsApp/urls.py
+++ ShiftsApp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import  staticfiles_urlpatterns
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Shifts/', include("Shifts.urls"))
+    path('Shifts/', include("Shifts.urls")),
+    
 ] 
+
+urlpatterns += staticfiles_urlpatterns()
