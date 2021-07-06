b9a7bb48453bbe6f76f0c758f3fe86b2e65c8f3e
leon trestman <tleonleont@gmail.com>
2021-07-06 15:49:26 +0300

    remove env

--- Shifts/views.py
+++ Shifts/views.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
@@ -11,15 +10,13 @@ from django.utils import timezone
 from django.contrib import messages
 
 
-# Create your views here.
+#index page
 def index(request):
-
-   # if not request.user.is_authenticated:
-    # return redirect("Shifts:login")
 
     return render(request, "Shifts/index.html")
 
 
+# login user page
 def login_view(request):
 
     if request.method == "POST":
@@ -30,108 +27,118 @@ def login_view(request):
             login(request, user)
             return redirect("Shifts:index")
         else:
-            return render(request, "Shifts/login.html", {
-                "message": "Invalid Input."
-            })
+            return render(request, "Shifts/login.html", {"message": "Invalid Input."})
 
     return render(request, "Shifts/login.html")
 
 
+# logout user page
 def logout_view(request):
     user_name = request.user.username  # get user name before logout
     logout(request)
-    return render(request, "Shifts/login.html", {
-        "message": f" {user_name} Logged out."
-    })
+    return render(
+        request, "Shifts/login.html", {"message": f" {user_name} Logged out."}
+    )
 
 
-#adding shift to the database
-#the shift contains name and a week schedule of the worker
+# adding shifts/ updating weekly shifts (if they exsit)
+# the shifts contain the praiorities of the user as well as creation/update data
 @login_required()
-def addshift(request):
+def add_shifts(request):
 
     if request.method == "POST":
-        form = shiftSubmitTwoForm(request.POST)
+
+        weekly_user_shift=get_weekly_user_shifts(request)
+        # if weekly shift from user doesn't exist create a new form
+        if (weekly_user_shift == None):
+            form = shiftSubmitTwoForm(request.POST)
+        #update the weekly shifts
+        else: 
+            form = shiftSubmitTwoForm(request.POST,instance=weekly_user_shift)
 
         if form.is_valid():
-            #adding authenticed user into the model
-            #login is required so  user always not null
+            # adding authenticed user into the model
+            # login is required so  user always not null
             shift_submit = form.save(commit=False)
             shift_submit.user_name = request.user
             shift_submit.save()
 
         else:
-            return render(request, "Shifts/addshift.html", {
-                "form": form
-            })
+            return render(request, "Shifts/addshifts.html", {"form": form})
 
-    #landing page before POST
-    return render(request, "Shifts/addshift.html", {
-        "form": shiftSubmitTwoForm()
-    })
-
-#changeing the weekly shift
-#if user has no weekly shift,he will be redriected to adding shhift with a msg of no weekly shifts
+    # landing page before POST
+    # to do here
+    return render(request, "Shifts/addshifts.html", {"form": shiftSubmitTwoForm()})
 
 
-@login_required()
-def myweekshift(request):
 
-    #calculating this week
+# returns user weekly shift from the database
+def get_weekly_user_shifts(request):
+    # calculating this week
     week_start = timezone.now()
     # for a week that starts on a Sunday
-    week_start -= timedelta(days=(week_start.weekday()-1) % 7)
+    week_start -= timedelta(days=(week_start.weekday() - 1) % 7)
     # 7 days for satruday week-end change to 5 if you want untll thorsday
     week_end = week_start + timedelta(days=7)
-    user_shift_result = shiftSubmitTwo.objects.filter(user_name=request.user, created_at__gte=week_start,
-                                                      created_at__lt=week_end).last()
-    if user_shift_result == None:  # if weekly shift from user doesn't exist redireact to add shift
+    return shiftSubmitTwo.objects.filter(
+        user_name=request.user, created_at__gte=week_start, created_at__lt=week_end
+    ).last()
+
+
+# shows user weekly shift if available, otherwise redirects to adding shift
+@login_required()
+def myweekshift(request):
+    user_shift_result = get_weekly_user_shifts(request)
+     # if weekly shift from user doesn't exist redireact to add shift
+    if (
+        user_shift_result == None
+    ): 
         # msg of error no shift schedule
-        messages.error(
-            request, f"{request.user} has no weekly shifts schedule.")
-        return redirect("Shifts:addshift")
+        messages.error(request, f"{request.user} has no weekly shifts schedule.")
+        return redirect("Shifts:addshifts")
 
-    return render(request, "Shifts/myweekshift.html", {
-        "user_shift": user_shift_result
-    })
+    return render(request, "Shifts/myweekshift.html", {"user_shift": user_shift_result})
 
 
-#
-#todo chhange my weekly shift
-#
 
 
-#all Shifts display for staff
-#optional pass up to days ago to get all shifts up to x days ago from now
+# all Shifts display for staff
+# optional pass up to days ago to get all shifts up to x days ago from now
 @staff_member_required()
 def all_shifts(request, up_to_days_ago=None):
 
-    if (not up_to_days_ago == None):  # if up_to_days_ago exsits than query  up to x days ago from now
+    if (
+        not up_to_days_ago == None
+    ):  # if up_to_days_ago exsits than query  up to x days ago from now
         time_threshold = timezone.now() - timedelta(days=up_to_days_ago)
         all_shift_res = shiftSubmitTwo.objects.filter(
-            created_at__gte=time_threshold)  # filter query up to x days ago from now
+            created_at__gte=time_threshold
+        )  # filter query up to x days ago from now
     else:
         all_shift_res = shiftSubmitTwo.objects.all()
-    return render(request, "Shifts/all_shifts.html", {
-        "all_shifts": all_shift_res
-    })
+    return render(request, "Shifts/all_shifts.html", {"all_shifts": all_shift_res})
 
 
-#return all shifts of certain user by his name
-#optional pass up to days ago to get all shifts from user up to x days ago from now
+# return all shifts of certain user by his name
+# optional pass up to days ago to get all shifts from user up to x days ago from now
 @staff_member_required()
 def user_shifts(request, user_name, up_to_days_ago=None):
     # get user by user_name or raise 404 if doesn't exsit
     uid = get_object_or_404(User, username=f"{user_name}")
-    if (not up_to_days_ago == None):  # if up_to_days_ago exsits than query up to x days ago from now
+    if (
+        not up_to_days_ago == None
+    ):  # if up_to_days_ago exsits than query up to x days ago from now
         time_threshold = timezone.now() - timedelta(days=up_to_days_ago)
         user_shift_result = shiftSubmitTwo.objects.filter(
-            user_name=f"{uid.id}", created_at__gte=time_threshold)
+            user_name=f"{uid.id}", created_at__gte=time_threshold
+        )
     else:  # display of all the user shifts
         user_shift_result = shiftSubmitTwo.objects.filter(
-            user_name=f"{uid.id}")  # filter with id of user
+            user_name=f"{uid.id}"
+        )  # filter with id of user
 
-    return render(request, "Shifts/user_shifts.html", {
-        "shift": user_shift_result,
-        "user_name": user_name
-    })
+    return render(
+        request,
+        "Shifts/user_shifts.html",
+        {"shift": user_shift_result, "user_name": user_name},
+    )
